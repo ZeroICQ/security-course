f0fb00af03a6b9c4168ead41a1363b85740a545b
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-09-19 23:03:43 +1000

    hw2, hw3

---
+++ HelloWorld2/main.c
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hIinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLinem, int nCmdShow)
+{
+    MessageBox(0, "Hello world", "Caption", 0);
+    return 0;
+}
---
+++ HelloWorld3/main.c
@@ -0,0 +1,40 @@
+﻿#define STRICT
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#pragma comment (linker,"-merge:.rdata=.text")
+const CHAR szMessage[]="Hello, world!";
+
+void CenterText(HDC hDC,int x,int y,LPCTSTR szMessage,int point)
+{
+    HFONT hFont=CreateFont(
+            point * GetDeviceCaps(hDC, LOGPIXELSY) / 60,
+            0, 370, 0, FW_BOLD, TRUE, FALSE, FALSE,
+            RUSSIAN_CHARSET, OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS,
+            PROOF_QUALITY, VARIABLE_PITCH, "Times New Roman");      // Создаем новый шрифт
+    HGDIOBJ hOld = SelectObject(hDC, hFont);        // Сохраняем старый шрифт
+    SetTextAlign(hDC, TA_CENTER | TA_BASELINE);     // Текст выровняем по центру
+    SetBkMode(hDC, TRANSPARENT);    // Прозрачность, чтоб текст не оказался в квадратной рамке с цветом фона
+    SetTextColor(hDC, RGB(0x40,0xAA,0));    // Цвет текста
+    TextOut(hDC, x, y, szMessage, strlen(szMessage));       // Рисуем текст на поверхности
+    SelectObject(hDC, hOld);        // Возвращаем старый шрифт
+    DeleteObject(hFont);            // Удаляем наш кастомный шрифт
+}
+
+void WinMainCRTStartup()
+{
+    // HDC - это такая поверхность, по которой планируется рисовать
+    // В данном случае мы будем рисовать по поверхности всех имеющихся
+    // мониторов. А в общем случае она может быть создана и для принтера
+    // и для других экзотических устройств
+    HDC hDC = CreateDC("DISPLAY", NULL, NULL, NULL);
+    while(1)        // Повторять будем бесконечно
+    {
+        // Выводим текст
+        CenterText(hDC, GetSystemMetrics(SM_CXSCREEN) / 2,
+                GetSystemMetrics(SM_CYSCREEN) / 2, szMessage, 72);
+        Sleep(100);     // Ждем 1/10 секунды, чтоб не забивать процессор
+    }
+    // Сюда выполнение не дойдет, задачу нужно завершить через список процессов
+    ReleaseDC(NULL, hDC);
+    ExitProcess(0);
+}

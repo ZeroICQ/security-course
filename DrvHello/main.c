b4e2684b3fb559f5cb3579a157ceace244caf456
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-09-20 21:40:54 +1000

    DrvHello

---
+++ DrvHello/main.c
@@ -0,0 +1,11 @@
+﻿#include "ntddk.h"
+
+#pragma code_seg("INIT")
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject,
+    IN PUNICODE_STRING RegistryPath)
+{
+    // int a = *((int *)0);
+    DbgPrint("Hello, world!");                  // Выдача отладочного сообщения
+    return STATUS_DEVICE_CONFIGURATION_ERROR;   // Выдача ошибки заставит систему сразу же выгрузить драйвер
+}
+#pragma code_seg()
---
+++ DrvHelloloader/main.c
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib") 
+
+void main(int argc, char* argv[])
+{
+    char serviceName[] = "drvhello";
+    char driverBinary[MAX_PATH] = "";
+    SC_HANDLE hSc;
+    SC_HANDLE hService;
+
+    // Чтобы запустить драйвер, потребуется полный путь к нему
+    // Предполагаем, что он лежит в той же папке, что и экзешник
+    strcpy(driverBinary, argv[0]);                  // argv[0] - здесь будет имя экзешника с полным путем
+    PathRemoveFileSpec(driverBinary);               // Выкидываем имя экзешника, остается только путь к папке
+    strcat(driverBinary, "\\drvhello.sys"); // Добавляем имя драйвера.
+                                    // Бэкслэш в строках Си надо удваивать, из-за его специального значения.
+
+    hSc = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS); // Открываем SCM (Service Control Management)
+                                                            // Это такая штука, которая позволяет запускать драйверы
+                                                            // из пользовательского режима
+    CreateService(hSc, serviceName, serviceName,
+            SERVICE_ALL_ACCESS, SERVICE_KERNEL_DRIVER, SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL,
+            driverBinary, NULL, NULL, NULL, NULL, NULL);                            // Загрузка в 3 этапа - создаем службу
+    hService = OpenService(hSc, serviceName, SERVICE_ALL_ACCESS);   // Открываем ее
+    StartService(hService, 0, NULL);        // И запускаем. Вообще-то было бы неплохо
+                                            // еще и закрыть ее... Как нибудь потом.
+}

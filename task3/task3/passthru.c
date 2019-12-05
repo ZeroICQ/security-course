2d10f052537aeb0ecf9d49ef5b7a096afcd6aa10
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-12-05 21:27:12 +1000

    WIP

--- task3/task3/passthru.c
+++ task3/task3/passthru.c
@@ -1,458 +1,178 @@
-/*++
-
-Copyright (c) 1992-2000  Microsoft Corporation
- 
-Module Name:
- 
-    passthru.c
-
-Abstract:
-
-    Ndis Intermediate Miniport driver sample. This is a passthru driver.
-
-Author:
-
-Environment:
-
-
-Revision History:
-
-
---*/
-
-
 #include "precomp.h"
-#pragma hdrstop
 
-#pragma NDIS_INIT_FUNCTION(DriverEntry)
-
-NDIS_HANDLE         ProtHandle = NULL;
-NDIS_HANDLE         DriverHandle = NULL;
-NDIS_MEDIUM         MediumArray[4] =
-                    {
-                        NdisMedium802_3,    // Ethernet
-                        NdisMedium802_5,    // Token-ring
-                        NdisMediumFddi,     // Fddi
-                        NdisMediumWan       // NDISWAN
-                    };
-
-NDIS_SPIN_LOCK     GlobalLock;
-
-PADAPT             pAdaptList = NULL;
-LONG               MiniportCount = 0;
-
-NDIS_HANDLE        NdisWrapperHandle;
-
-//
-// To support ioctls from user-mode:
-//
-
-#define LINKNAME_STRING     L"\\DosDevices\\Passthru"
-#define NTDEVICE_STRING     L"\\Device\\Passthru"
-
-NDIS_HANDLE     NdisDeviceHandle = NULL;
-PDEVICE_OBJECT  ControlDeviceObject = NULL;
-
-enum _DEVICE_STATE
-{
-    PS_DEVICE_STATE_READY = 0,    // ready for create/delete
-    PS_DEVICE_STATE_CREATING,    // create operation in progress
-    PS_DEVICE_STATE_DELETING    // delete operation in progress
-} ControlDeviceState = PS_DEVICE_STATE_READY;
-
-
-
-NTSTATUS
-DriverEntry(
-    IN PDRIVER_OBJECT        DriverObject,
-    IN PUNICODE_STRING       RegistryPath
-    )
+NTSTATUS DriverEntry(
+    IN PDRIVER_OBJECT DriverObject,
+    IN PUNICODE_STRING RegistryPath
+)
 /*++
 
 Routine Description:
 
-    First entry point to be called, when this driver is loaded.
-    Register with NDIS as an intermediate driver.
-
 Arguments:
 
-    DriverObject - pointer to the system's driver object structure
-        for this driver
-    
-    RegistryPath - system's registry path for this driver
-    
 Return Value:
-
-    STATUS_SUCCESS if all initialization is successful, STATUS_XXX
-    error code if not.
 
 --*/
 {
-    NDIS_STATUS                        Status;
-    NDIS_PROTOCOL_CHARACTERISTICS      PChars;
-    NDIS_MINIPORT_CHARACTERISTICS      MChars;
-    NDIS_STRING                        Name;
+    NDIS_STATUS Status;
+    NDIS_PROTOCOL_CHARACTERISTICS PChars;
+    NDIS_MINIPORT_CHARACTERISTICS MChars;
+    PNDIS_CONFIGURATION_PARAMETER Param;
 
-    Status = NDIS_STATUS_SUCCESS;
-    NdisAllocateSpinLock(&GlobalLock);
+    NDIS_STRING Name;
+    NDIS_HANDLE WrapperHandle;
+    // Этот массив будет содержать все указатели функций для регистрации.
+    PDRIVER_DISPATCH MajorFunctions[IRP_MJ_MAXIMUM_FUNCTION + 1];
 
-    NdisMInitializeWrapper(&NdisWrapperHandle, DriverObject, RegistryPath, NULL);
+    NDIS_STRING ntDeviceName; //имя для вызова виртуального device-а для NT
+    NDIS_STRING win32DeviceName; // тоже для win32
+    PDEVICE_OBJECT deviceObject; // и объект.
 
-    do
-    {
-        //
-        // Register the miniport with NDIS. Note that it is the miniport
-        // which was started as a driver and not the protocol. Also the miniport
-        // must be registered prior to the protocol since the protocol's BindAdapter
-        // handler can be initiated anytime and when it is, it must be ready to
-        // start driver instances.
-        //
+    //
+    // Register the miniport with NDIS. Note that it is the miniport
+    // which was started as a driver and not the protocol. Also the miniport
+    // must be registered prior to the protocol since the protocol's BindAdapter
+    // handler can be initiated anytime and when it is, it must be ready to
+    // start driver instances.
+    //
 
-        NdisZeroMemory(&MChars, sizeof(NDIS_MINIPORT_CHARACTERISTICS));
+    // функция указывает системе NDIS, что пришло время инициировать miniport service в ее системе.
+    // Возвращаемое значение необходимо сохранить на будущее.
+    // Обязательно надо обратить внимание, что если происходит ошибка при инициализации любого объекта, 
+    // то при уже нормально отработавшей функции NdisMInitializeWrapper нужно вызвать NdisTerminateWrapper для высвобождения ресурса.
+    NdisMInitializeWrapper(&WrapperHandle, DriverObject, RegistryPath, NULL);
+    // function fills a block of memory with zeros.
+    NdisZeroMemory(&MChars, sizeof(NDIS_MINIPORT_CHARACTERISTICS));
 
-        MChars.MajorNdisVersion = PASSTHRU_MAJOR_NDIS_VERSION;
-        MChars.MinorNdisVersion = PASSTHRU_MINOR_NDIS_VERSION;
+    MChars.MajorNdisVersion = 4;
+    MChars.MinorNdisVersion = 0;
+    MChars.InitializeHandler = MPInitialize;
+    MChars.QueryInformationHandler = MPQueryInformation;
+    MChars.SetInformationHandler = MPSetInformation;
+    MChars.ResetHandler = MPReset;
+    MChars.TransferDataHandler = MPTransferData;
+    MChars.HaltHandler = MPHalt;
 
-        MChars.InitializeHandler = MPInitialize;
-        MChars.QueryInformationHandler = MPQueryInformation;
-        MChars.SetInformationHandler = MPSetInformation;
-        MChars.ResetHandler = NULL;
-        MChars.TransferDataHandler = MPTransferData;
-        MChars.HaltHandler = MPHalt;
-#ifdef NDIS51_MINIPORT
-        MChars.CancelSendPacketsHandler = MPCancelSendPackets;
-        MChars.PnPEventNotifyHandler = MPDevicePnPEvent;
-        MChars.AdapterShutdownHandler = MPAdapterShutdown;
-#endif // NDIS51_MINIPORT
+    //
+    // We will disable the check for hang timeout so we do not
+    // need a check for hang handler!
+    //
 
-        //
-        // We will disable the check for hang timeout so we do not
-        // need a check for hang handler!
-        //
-        MChars.CheckForHangHandler = NULL;
-        MChars.ReturnPacketHandler = MPReturnPacket;
+    MChars.CheckForHangHandler = NULL;
+    MChars.SendHandler = MPSend;
+    MChars.ReturnPacketHandler = MPReturnPacket;
 
-        //
-        // Either the Send or the SendPackets handler should be specified.
-        // If SendPackets handler is specified, SendHandler is ignored
-        //
-        MChars.SendHandler = NULL;    // MPSend;
-        MChars.SendPacketsHandler = MPSendPackets;
+    //
+    // Either the Send or the SendPackets handler should be specified.
+    // If SendPackets handler is specified, SendHandler is ignored
+    //
+    // MChars.SendPacketsHandler = MPSendPackets;
+    // Функция, регистрирующая все функции уровня miniport
+    Status = NdisIMRegisterLayeredMiniport(WrapperHandle, &MChars, sizeof(MChars), &DriverHandle);
+    ASSERT(Status == NDIS_STATUS_SUCCESS);
+    // хэдлер для правильной выгрузки функций группы miniport
+    NdisMRegisterUnloadHandler(WrapperHandle, PtUnload);
 
-        Status = NdisIMRegisterLayeredMiniport(NdisWrapperHandle,
-                                                  &MChars,
-                                                  sizeof(MChars),
-                                                  &DriverHandle);
-        if (Status != NDIS_STATUS_SUCCESS)
-        {
-            break;
-        }
+    //
+    // Now register the protocol.
+    //
 
-#ifndef WIN9X
-        NdisMRegisterUnloadHandler(NdisWrapperHandle, PtUnload);
-#endif
+    NdisZeroMemory(&PChars, sizeof(NDIS_PROTOCOL_CHARACTERISTICS));
+    PChars.MajorNdisVersion = 4;
+    PChars.MinorNdisVersion = 0;
 
-        //
-        // Now register the protocol.
-        //
-        NdisZeroMemory(&PChars, sizeof(NDIS_PROTOCOL_CHARACTERISTICS));
-        PChars.MajorNdisVersion = PASSTHRU_PROT_MAJOR_NDIS_VERSION;
-        PChars.MinorNdisVersion = PASSTHRU_PROT_MINOR_NDIS_VERSION;
+    //
+    // Make sure the protocol-name matches the service-name under which this protocol is installed.
+    // This is needed to ensure that NDIS can correctly determine the binding and call us to bind
+    // to miniports below.
+    //
 
-        //
-        // Make sure the protocol-name matches the service-name
-        // (from the INF) under which this protocol is installed.
-        // This is needed to ensure that NDIS can correctly determine
-        // the binding and call us to bind to miniports below.
-        //
-        NdisInitUnicodeString(&Name, L"Passthru");    // Protocol name
-        PChars.Name = Name;
-        PChars.OpenAdapterCompleteHandler = PtOpenAdapterComplete;
-        PChars.CloseAdapterCompleteHandler = PtCloseAdapterComplete;
-        PChars.SendCompleteHandler = PtSendComplete;
-        PChars.TransferDataCompleteHandler = PtTransferDataComplete;
-    
-        PChars.ResetCompleteHandler = PtResetComplete;
-        PChars.RequestCompleteHandler = PtRequestComplete;
-        PChars.ReceiveHandler = PtReceive;
-        PChars.ReceiveCompleteHandler = PtReceiveComplete;
-        PChars.StatusHandler = PtStatus;
-        PChars.StatusCompleteHandler = PtStatusComplete;
-        PChars.BindAdapterHandler = PtBindAdapter;
-        PChars.UnbindAdapterHandler = PtUnbindAdapter;
-        PChars.UnloadHandler = PtUnloadProtocol;
+    NdisInitUnicodeString(&Name, L"SFilter"); // Protocol name
+    PChars.Name = Name;
+    PChars.OpenAdapterCompleteHandler = PtOpenAdapterComplete;
+    PChars.CloseAdapterCompleteHandler = PtCloseAdapterComplete;
+    PChars.SendCompleteHandler = PtSendComplete;
+    PChars.TransferDataCompleteHandler = PtTransferDataComplete;
+    PChars.ResetCompleteHandler = PtResetComplete;
+    PChars.RequestCompleteHandler = PtRequestComplete;
+    PChars.ReceiveHandler = PtReceive;
+    PChars.ReceiveCompleteHandler = PtReceiveComplete;
+    PChars.StatusHandler = PtStatus;
+    PChars.StatusCompleteHandler = PtStatusComplete;
+    PChars.BindAdapterHandler = PtBindAdapter;
+    PChars.UnbindAdapterHandler = PtUnbindAdapter;
+    PChars.UnloadHandler = NULL;
+    PChars.ReceivePacketHandler = PtReceivePacket;
+    PChars.PnPEventHandler = PtPNPHandler;
+    // Функция, регистрирующая все функции протокола. Протокольная группа
+    NdisRegisterProtocol(&Status, &ProtHandle, &PChars, sizeof(NDIS_PROTOCOL_CHARACTERISTICS));
 
-        PChars.ReceivePacketHandler = PtReceivePacket;
-        PChars.PnPEventHandler= PtPNPHandler;
+    ASSERT(Status == NDIS_STATUS_SUCCESS);
 
-        NdisRegisterProtocol(&Status,
-                             &ProtHandle,
-                             &PChars,
-                             sizeof(NDIS_PROTOCOL_CHARACTERISTICS));
+    // Name of control deviceObject.
+    // DeviceName that names the device object.
+    NdisInitUnicodeString(&ntDeviceName, L"\Device\passthru");
 
-        if (Status != NDIS_STATUS_SUCCESS)
-        {
-            NdisIMDeregisterLayeredMiniport(DriverHandle);
-            break;
-        }
+    // SymbolicName that is the Win32-visible name of the device
+    NdisInitUnicodeString(&win32DeviceName, L"\DosDevices\passthru");
+    //Создаем строку имени
 
-        NdisIMAssociateMiniport(DriverHandle, ProtHandle);
-    }
-    while (FALSE);
-
+    NdisZeroMemory(MajorFunctions, sizeof(MajorFunctions));
+    //Связываем имена функций с массивом
+    MajorFunctions[IRP_MJ_CREATE] = FilterOpen;
+    MajorFunctions[IRP_MJ_CLOSE] = FilterClose;
+    MajorFunctions[IRP_MJ_READ] = FilterRead;
+    MajorFunctions[IRP_MJ_WRITE] = FilterWrite;
+    MajorFunctions[IRP_MJ_DEVICE_CONTROL] = FilterIoControl;
+    //Регистрируем их
+    Status = NdisMRegisterDevice(WrapperHandle, &ntDeviceName,
+        &win32DeviceName, MajorFunctions,
+        &deviceObject, &GlobalData.NdisDeviceHandle);
+    // проверяем статус
     if (Status != NDIS_STATUS_SUCCESS)
     {
-        NdisTerminateWrapper(NdisWrapperHandle, NULL);
+        if (GlobalData.ProtHandle)
+            NdisDeregisterProtocol(&Status, GlobalData.ProtHandle);
+
+        if (GlobalData.NdisDeviceHandle)
+            NdisMDeregisterDevice(GlobalData.NdisDeviceHandle);
+
+        if (WrapperHandle)
+            NdisTerminateWrapper(WrapperHandle, NULL);
+        return (Status);
     }
+
+    // set access method into deviceObject ( received from NdisMRegisterDevice() )
+    // объявление буферизации для связывающих операций
+    deviceObject->Flags |= DO_BUFFERED_IO;
+
+
+    // Функция, информирующая NDIS о том, что есть, существует два уровня, минипорт и протокол, и говорящая об экспорте функций, если таковой присутствует.
+    // Связываем протокол и минипорт
+    NdisIMAssociateMiniport(DriverHandle, ProtHandle);
 
     return(Status);
 }
 
-
-NDIS_STATUS
-PtRegisterDevice(
-    VOID
-    )
-/*++
-
-Routine Description:
-
-    Register an ioctl interface - a device object to be used for this
-    purpose is created by NDIS when we call NdisMRegisterDevice.
-
-    This routine is called whenever a new miniport instance is
-    initialized. However, we only create one global device object,
-    when the first miniport instance is initialized. This routine
-    handles potential race conditions with PtDeregisterDevice via
-    the ControlDeviceState and MiniportCount variables.
-
-    NOTE: do not call this from DriverEntry; it will prevent the driver
-    from being unloaded (e.g. on uninstall).
-
-Arguments:
-
-    None
-
-Return Value:
-
-    NDIS_STATUS_SUCCESS if we successfully register a device object.
-
---*/
+NTSTATUS FilterOpen(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
 {
-    NDIS_STATUS            Status = NDIS_STATUS_SUCCESS;
-    UNICODE_STRING         DeviceName;
-    UNICODE_STRING         DeviceLinkUnicodeString;
-    PDRIVER_DISPATCH       DispatchTable[IRP_MJ_MAXIMUM_FUNCTION+1];
-
-    DBGPRINT(("==>PtRegisterDevice\n"));
-
-    NdisAcquireSpinLock(&GlobalLock);
-
-    ++MiniportCount;
-    
-    if (1 == MiniportCount)
-    {
-        ASSERT(ControlDeviceState != PS_DEVICE_STATE_CREATING);
-
-        //
-        // Another thread could be running PtDeregisterDevice on
-        // behalf of another miniport instance. If so, wait for
-        // it to exit.
-        //
-        while (ControlDeviceState != PS_DEVICE_STATE_READY)
-        {
-            NdisReleaseSpinLock(&GlobalLock);
-            NdisMSleep(1);
-            NdisAcquireSpinLock(&GlobalLock);
-        }
-
-        ControlDeviceState = PS_DEVICE_STATE_CREATING;
-
-        NdisReleaseSpinLock(&GlobalLock);
-
-    
-        NdisZeroMemory(DispatchTable, (IRP_MJ_MAXIMUM_FUNCTION+1) * sizeof(PDRIVER_DISPATCH));
-
-        DispatchTable[IRP_MJ_CREATE] = PtDispatch;
-        DispatchTable[IRP_MJ_CLEANUP] = PtDispatch;
-        DispatchTable[IRP_MJ_CLOSE] = PtDispatch;
-        DispatchTable[IRP_MJ_DEVICE_CONTROL] = PtDispatch;
-        
-
-        NdisInitUnicodeString(&DeviceName, NTDEVICE_STRING);
-        NdisInitUnicodeString(&DeviceLinkUnicodeString, LINKNAME_STRING);
-
-        //
-        // Create a device object and register our dispatch handlers
-        //
-        
-        Status = NdisMRegisterDevice(
-                    NdisWrapperHandle, 
-                    &DeviceName,
-                    &DeviceLinkUnicodeString,
-                    &DispatchTable[0],
-                    &ControlDeviceObject,
-                    &NdisDeviceHandle
-                    );
-
-        NdisAcquireSpinLock(&GlobalLock);
-
-        ControlDeviceState = PS_DEVICE_STATE_READY;
-    }
-
-    NdisReleaseSpinLock(&GlobalLock);
-
-    DBGPRINT(("<==PtRegisterDevice: %x\n", Status));
-
-    return (Status);
+    return NDIS_STATUS_SUCCESS;
 }
 
-
-NTSTATUS
-PtDispatch(
-    IN PDEVICE_OBJECT    DeviceObject,
-    IN PIRP              Irp
-    )
-/*++
-Routine Description:
-
-    Process IRPs sent to this device.
-
-Arguments:
-
-    DeviceObject - pointer to a device object
-    Irp      - pointer to an I/O Request Packet
-
-Return Value:
-
-    NTSTATUS - STATUS_SUCCESS always - change this when adding
-    real code to handle ioctls.
-
---*/
+NTSTATUS FilterClose(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
 {
-    PIO_STACK_LOCATION  irpStack;
-    NTSTATUS            status = STATUS_SUCCESS;
-
-    UNREFERENCED_PARAMETER(DeviceObject);
-    
-    DBGPRINT(("==>Pt Dispatch\n"));
-    irpStack = IoGetCurrentIrpStackLocation(Irp);
-      
-
-    switch (irpStack->MajorFunction)
-    {
-        case IRP_MJ_CREATE:
-            break;
-            
-        case IRP_MJ_CLEANUP:
-            break;
-            
-        case IRP_MJ_CLOSE:
-            break;        
-            
-        case IRP_MJ_DEVICE_CONTROL:
-            //
-            // Add code here to handle ioctl commands sent to passthru.
-            //
-            break;        
-        default:
-            break;
-    }
-
-    Irp->IoStatus.Status = status;
-    IoCompleteRequest(Irp, IO_NO_INCREMENT);
-
-    DBGPRINT(("<== Pt Dispatch\n"));
-
-    return status;
-
-} 
-
-
-NDIS_STATUS
-PtDeregisterDevice(
-    VOID
-    )
-/*++
-
-Routine Description:
-
-    Deregister the ioctl interface. This is called whenever a miniport
-    instance is halted. When the last miniport instance is halted, we
-    request NDIS to delete the device object
-
-Arguments:
-
-    NdisDeviceHandle - Handle returned by NdisMRegisterDevice
-
-Return Value:
-
-    NDIS_STATUS_SUCCESS if everything worked ok
-
---*/
-{
-    NDIS_STATUS Status = NDIS_STATUS_SUCCESS;
-
-    DBGPRINT(("==>PassthruDeregisterDevice\n"));
-
-    NdisAcquireSpinLock(&GlobalLock);
-
-    ASSERT(MiniportCount > 0);
-
-    --MiniportCount;
-    
-    if (0 == MiniportCount)
-    {
-        //
-        // All miniport instances have been halted. Deregister
-        // the control device.
-        //
-
-        ASSERT(ControlDeviceState == PS_DEVICE_STATE_READY);
-
-        //
-        // Block PtRegisterDevice() while we release the control
-        // device lock and deregister the device.
-        // 
-        ControlDeviceState = PS_DEVICE_STATE_DELETING;
-
-        NdisReleaseSpinLock(&GlobalLock);
-
-        if (NdisDeviceHandle != NULL)
-        {
-            Status = NdisMDeregisterDevice(NdisDeviceHandle);
-            NdisDeviceHandle = NULL;
-        }
-
-        NdisAcquireSpinLock(&GlobalLock);
-        ControlDeviceState = PS_DEVICE_STATE_READY;
-    }
-
-    NdisReleaseSpinLock(&GlobalLock);
-
-    DBGPRINT(("<== PassthruDeregisterDevice: %x\n", Status));
-    return Status;
-    
+    return NDIS_STATUS_SUCCESS;
 }
 
-VOID
-PtUnload(
-    IN PDRIVER_OBJECT        DriverObject
-    )
-//
-// PassThru driver unload function
-//
+NTSTATUS FilterRead(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
 {
-    UNREFERENCED_PARAMETER(DriverObject);
-    
-    DBGPRINT(("PtUnload: entered\n"));
-    
-    PtUnloadProtocol();
-    
-    NdisIMDeregisterLayeredMiniport(DriverHandle);
-    
-    NdisFreeSpinLock(&GlobalLock);
-
-    DBGPRINT(("PtUnload: done!\n"));
+    return NDIS_STATUS_SUCCESS;
 }
-
+NTSTATUS FilterWrite(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
+{
+    return NDIS_STATUS_SUCCESS;
+}
+NTSTATUS FilterIoControl(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
+{
+    return NDIS_STATUS_SUCCESS;
+}
